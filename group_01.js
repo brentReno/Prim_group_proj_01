022c513c4e15c5bae30e2c1697a6f02e9eaac925
Brent Reno <brent.reno@gmail.com>
2016-08-25 17:19:50 -0500

    succesfull STI calculation switch

--- group_01.js
+++ group_01.js
@@ -7,62 +7,41 @@ var employees = [atticus, jem, boo, scout];
 
 var employeeBonus = [];
 // function that takes in an employee array
-var percent;
 
+var i = 3;
+var  percent = 0;
 
-//scope rating var
-
-
-
-
-  //switches that determine base bonus based on rating
-  var calculateSti = function(){
-      console.log('in the CalculateSti function');
-      var rating = employees[ i ] [3];
-      var basePay = parseInt(employees [ i ] [ 2 ]);
-      var employeeNum = (employees [ i ] [ 1 ]).length;
-
-    //begin switch
-    var stiCalc = function() {
-      switch( rating ) {
-          case(rating == 3):
-            percent =  0.04;
-            console.log(percent);
-          break;
-          case( rating == 4):
-            percent = 0.06;
-            console.log(percent);
-          break;
-          case( rating == 5):
-            percent = 0.1;
-            console.log(percent);
-          break;
-        }
-          return percent;
-      };
-      console.log('after switch:',percent);
-        var stiNum = stiCalc();
-
-        if(basePay > 65000) {
-
-        }
-        if(employeeNum == 4) {
-
-        }
-
-  };
-
-  for (var i = 0; i < employees.length; i++) {
-    console.log(calculateSti(employees));
+var calculateSTI = function() {
+  switch( employees[i][3] ) {
+    case(3):
+        percent =  0.04;
+        console.log(percent);
+      break;
+      case(4):
+        percent = 0.06;
+        console.log(percent);
+      break;
+      case(5):
+        percent = 0.1;
+        console.log(percent);
+      break;
   }
-  //if annaul income > 65,000
-  //if statement about length of employent
+console.log(percent);
 
-  //if greater than 13% = 13%
+if( employees[i][1].length == 4) {
+  percent += 0.05;
+  }
+console.log(percent);
 
+if( parseInt(employees[i][2]) > 65000) {
+  percent -= 0.01;
+}
+console.log(percent);
 
+if( percent > 0.13) {
+  percent = 0.13;
+}
+console.log(percent);
+};
 
-
-//Return an Array
-  //first item Employee Name
-  //
+calculateSTI();
